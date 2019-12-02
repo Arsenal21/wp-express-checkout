07a6e2860d2f11d8ad0b84234deacfe66b27a712
dikiyforester <frolov.en@gmail.com>
2019-12-02 19:27:02 +0500

    Send the notification email after a payment. Closes #2

--- wp-express-checkout/includes/class-wpec-process-ipn.php
+++ wp-express-checkout/includes/class-wpec-process-ipn.php
@@ -44,6 +44,9 @@ class WPEC_Process_IPN {
 	 * Processes the payment on AJAX call.
 	 */
 	public function wpec_process_payment() {
+
+		// TODO: AJAX Nonce verification.
+
 		if ( ! isset( $_POST['wp_ppdg_payment'] ) ) {
 			// no payment data provided.
 			_e( 'No payment data received.', 'paypal-express-checkout' );
@@ -98,7 +101,7 @@ class WPEC_Process_IPN {
 		// let's insert order.
 		$order = OrdersWPEC::get_instance();
 
-		$order->insert(
+		$order_id = $order->insert(
 			array(
 				'item_name'   => $item_name,
 				'price'       => $price,
@@ -112,7 +115,66 @@ class WPEC_Process_IPN {
 			$payment['payer']
 		);
 
-		// TODO - Send email notifications.
+		$wpec_plugin = WPEC_Main::get_instance();
+
+		$product_details = $item_name . ' x ' . $quantity . ' - ' . number_format( $amount, 2, '.', '' ) . ' ' . $currency . "\n";
+		/* translators:  %s - download link */
+		$product_details .= sprintf( __( 'Download Link: %s', 'paypal-express-checkout' ), base64_decode( $url ) ) . "\n";
+
+		$address = '';
+		if ( ! empty( $payment['purchase_units'][0]['shipping']['address'] ) ) {
+			$address = implode( ', ', (array) $payment['purchase_units'][0]['shipping']['address'] );
+		}
+
+		$args = array(
+			'first_name'      => $payment['payer']['name']['given_name'],
+			'last_name'       => $payment['payer']['name']['surname'],
+			'product_details' => $product_details,
+			'payer_email'     => $payment['payer']['email_address'],
+			'transaction_id'  => $payment['id'],
+			'purchase_amt'    => $amount,
+			'purchase_date'   => date( 'Y-m-d' ),
+			'coupon_code'     => '', // Seems like not implemented yet.
+			'address'         => $address,
+			'order_id'        => $order_id,
+		);
+
+		// Send email to buyer if needs.
+		if ( $wpec_plugin->get_setting( 'send_buyer_email' ) && ! empty( $payment['payer']['email_address'] ) ) {
+
+			$buyer_email = $payment['payer']['email_address'];
+			$from_email  = $wpec_plugin->get_setting( 'buyer_from_email' );
+			$subject     = $wpec_plugin->get_setting( 'buyer_email_subj' );
+			$subject     = $this->apply_dynamic_tags( $subject, $args );
+			$body        = $wpec_plugin->get_setting( 'buyer_email_body' );
+
+			$args['email_body'] = $body;
+
+			$body = $this->apply_dynamic_tags( $body, $args );
+			$body = apply_filters( 'wpec_buyer_notification_email_body', $body, $payment, $args );
+
+			$headers = 'From: ' . $from_email . "\r\n";
+
+			wp_mail( $buyer_email, $subject, $body, $headers );
+
+			update_post_meta( $order_id, 'wpsc_buyer_email_sent', 'Email sent to: ' . $buyer_email );
+		}
+
+		// Send email to seller if needs.
+		if ( $wpec_plugin->get_setting( 'send_seller_email' ) && ! empty( $wpec_plugin->get_setting( 'notify_email_address' ) ) ) {
+
+			$notify_email = $wpec_plugin->get_setting( 'notify_email_address' );
+
+			$seller_email_subject = $wpec_plugin->get_setting( 'seller_email_subj' );
+			$seller_email_subject = $this->apply_dynamic_tags( $seller_email_subject, $args );
+
+			$seller_email_body = $wpec_plugin->get_setting( 'seller_email_body' );
+			$seller_email_body = $this->apply_dynamic_tags( $seller_email_body, $args );
+			$seller_email_body = apply_filters( 'wpec_seller_notification_email_body', $seller_email_body, $payment, $args );
+
+			wp_mail( $notify_email, $seller_email_subject, $seller_email_body, $headers );
+		}
+
 		// Trigger the action hook.
 		do_action( 'wpec_payment_completed', $payment );
 
@@ -131,6 +193,42 @@ class WPEC_Process_IPN {
 		echo wp_json_encode( $res );
 
 		exit;
+	}
+
+	/**
+	 * Replaces tags in the text with appropriate values.
+	 *
+	 * @param string $text The text with tags to be replaced.
+	 * @param array  $args The array of the tags values.
+	 *
+	 * @return string
+	 */
+	private function apply_dynamic_tags( $text, $args ) {
+
+		$white_list = array(
+			'first_name',
+			'last_name',
+			'product_details',
+			'payer_email',
+			'transaction_id',
+			'purchase_amt',
+			'purchase_date',
+			'coupon_code',
+			'address',
+			'order_id',
+		);
+
+		$tags = array();
+		$vals = array();
+
+		foreach ( $white_list as $item ) {
+			$tags[] = "{{$item}}";
+			$vals[] = ( isset( $args[ $item ] ) ) ? $args[ $item ] : '';
+		}
+
+		$body = stripslashes( str_replace( $tags, $vals, $text ) );
+
+		return $body;
 	}
 
 }
