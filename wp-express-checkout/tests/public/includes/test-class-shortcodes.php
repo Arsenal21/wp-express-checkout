e7313bdedd0ba72b6385e610a0202eb2ec0f7ce8
forester <arney.frazer@techork.com>
2021-12-30 08:55:32 +0500

    Updated tests

--- wp-express-checkout/tests/public/includes/test-class-shortcodes.php
+++ wp-express-checkout/tests/public/includes/test-class-shortcodes.php
@@ -487,6 +487,7 @@ class ShortcodesTest extends \WP_UnitTestCase {
 			'use_modal'       => $defaults['use_modal'],
 			'thank_you_url'   => $defaults['thank_you_url'],
 			'variations'      => [],
+			'price_class'     => 'wpec-price-' . substr( sha1( time() . mt_rand( 0, 1000 ) ), 0, 10 ),
 		];
 		return [
 			[ $args, [ 'is_live' => 0, 'sandbox_client_id' => 'test' ] ],
