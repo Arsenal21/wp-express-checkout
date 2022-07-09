21d262dd4ae2abc7df719370eb7657750898206e
Muneeb <imuneeb@me.com>
2022-07-09 15:28:18 +0500

    Update page-link.php

--- wp-express-checkout/public/views/templates/page-link.php
+++ wp-express-checkout/public/views/templates/page-link.php
@@ -1,8 +1,46 @@
 <html>
     <head>
 
-        <?php wp_head() ?>
+        <?php 
 
+            require WPEC_PLUGIN_PATH . '/vendor/autoload.php';
+
+            global $wp_scripts;
+
+            $wp_scripts->print_scripts("jquery");
+
+            $min = ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ? '' : '.min';
+
+            $scriptFrontEnd = WPEC_PLUGIN_URL . "/assets/js/public{$min}.js";
+            $styleFrontEnd = WPEC_PLUGIN_URL . "/assets/css/public{$min}.css";
+            $localVars  = array(
+                'str' => array(
+                    'errorOccurred'    => __( 'Error occurred', 'wp-express-checkout' ),
+                    'paymentFor'       => __( 'Payment for', 'wp-express-checkout' ),
+                    'enterQuantity'    => __( 'Please enter a valid quantity', 'wp-express-checkout' ),
+                    'stockErr'         => __( 'You cannot order more items than available: %d', 'wp-express-checkout' ),
+                    'enterAmount'      => __( 'Please enter a valid amount', 'wp-express-checkout' ),
+                    'acceptTos'        => __( 'Please accept the terms and conditions', 'wp-express-checkout' ),
+                    'paymentCompleted' => __( 'Payment Completed', 'wp-express-checkout' ),
+                    'redirectMsg'      => __( 'You are now being redirected to the order summary page.', 'wp-express-checkout' ),
+                    'strRemoveCoupon'  => __( 'Remove coupon', 'wp-express-checkout' ),
+                    'strRemove'        => __( 'Remove', 'wp-express-checkout' ),
+                    'required'         => __( 'This field is required', 'wp-express-checkout' ),
+                ),
+                'ajaxUrl' => get_admin_url() . 'admin-ajax.php',
+            );
+
+        ?>
+
+        <link rel="stylesheet" href="<?php echo $styleFrontEnd ?>" />
+
+        <script type="text/javascript">
+            var ppecFrontVars = <?php echo json_encode($localVars) ?>
+        </script>
+
+        <script src="<?php echo $scriptFrontEnd ?>"></script>
+        
+        
         <style>
             #wp-express-checkout-button-cont .wpec-modal-open {
                 display: none;
@@ -21,7 +59,6 @@
             }
 
             #wp-express-checkout-error-product-id {
-                
                 text-align: center;
                 font-size: 18px;
                 background: red;
@@ -63,5 +100,5 @@
             </div>
         <?php endif; ?>
     </body>
-    <?php wp_footer() ?>
+    <?php WP_Express_Checkout\Main::get_instance()->load_paypal_sdk() ?>
 </html>
