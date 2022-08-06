1c0cf80c80f0de9c03dc6c8b36ee4317303af104
Muneeb <imuneeb@me.com>
2022-08-06 19:09:05 +0500

    Template 3 added.

--- wp-express-checkout/public/views/templates/content-product-3.php
+++ wp-express-checkout/public/views/templates/content-product-3.php
@@ -16,12 +16,12 @@ $wpec_button_args["button_text"] = __( "Buy Now" , "wp-express-checkout" );
     <div class="wpec-product-inner-cont-template-3">
         <div class="wpec-product-item-thumbnail-3">
             <?php if ( ! empty( $wpec_button_args['thumbnail_url'] ) ) { ?>
-				<img width="150" height="150" src="<?php echo esc_url( $wpec_button_args['thumbnail_url'] ); ?>" class="attachment-thumbnail size-thumbnail wp-post-image" alt="">
-			<?php } ?>
-		</div>
-		<div class="wpec-post-title wpec-post-title-template-3">
-			<?php the_title(); ?>
-		</div>
+                <img width="150" height="150" src="<?php echo esc_url( $wpec_button_args['thumbnail_url'] ); ?>" class="attachment-thumbnail size-thumbnail wp-post-image" alt="">
+            <?php } ?>
+        </div>
+        <div class="wpec-post-title wpec-post-title-template-3">
+            <?php the_title(); ?>
+        </div>
 
     
         
@@ -35,43 +35,3 @@ $wpec_button_args["button_text"] = __( "Buy Now" , "wp-express-checkout" );
         </div>
     </div>
 </div>
-
-
-<style>
-
-.wpec-product-item-thumbnail-3 {
-   
-}
-.wpec-post-title-template-3 {
-    font-size: 16px;
-    margin-bottom: 0px;
-   
-}
-.wpec-product-item-template-3 {
-  
-	width: 250px;
-    
-}
-
-.wpec-product-inner-cont-template-3 {
-    text-align: center;
-}
-
-.wpec-product-inner-cont-template-3 .wpec-modal-open {
-    background-color: #fff;
-    border-radius: 4px;
-    font-weight: 700;
-    padding-top: 8px;
-    padding-bottom: 8px;
-    padding-left: 16px;
-    padding-right: 16px;
-    color: #000;
-}
-
-.wpec-product-inner-cont-template-3 .wpec-modal-open:hover {
-    background-color: #f0f0f0;
-    color: #000;
-    font-weight: bold;
-    cursor: pointer;
-}
-</style>
