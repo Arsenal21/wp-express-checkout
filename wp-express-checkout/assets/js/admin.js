b2ed722c61c756eadaead61291c9b04f56e7395b
sunjava11 <sunjava11@gmail.com>
2023-07-13 16:41:41 +0500

    admin order note feature

--- wp-express-checkout/assets/js/admin.js
+++ wp-express-checkout/assets/js/admin.js
@@ -1,6 +1,116 @@
 ( function( $ ) {
 	"use strict";
 
+	 // Add order note
+	 $('#order-note-form-submit').on('click', function(e) {
+        e.preventDefault();
+
+		var btn = $(this);
+
+        var note = $('#wpec_order_note').val();
+		var order_id = $('#wpec_order_id').val();
+
+		if(!note || note=="")
+		{
+			alert("Please add some notes first");
+			return;
+		}
+
+		btn.val("adding");
+		btn.prop("disabled",true);
+
+        if (note !== '') {
+            var data = {
+                action: 'wpec_add_order_note',
+                note: note,
+				order_id: order_id,
+				nonce: wpecAdminSideVars.add_order_note_nonance
+            };
+
+            $.ajax({
+                url: wpecAdminSideVars.ajaxurl,
+                type: 'POST',
+                data: data,
+                success: function(response) {
+					btn.val("Add");
+					btn.prop("disabled",false);
+                    // Handle success response
+                    if (response.success) {
+						
+						// Clear the note input
+						$("#wpec_order_note").val("");
+                        
+                        // Append the new note to the table
+                        var noteRow = '<div class="wpec-single-note" id="wpec_single_note_'+response.data.note.id+'">' +
+                        '<p>' + response.data.note.content + '</p>' +
+						'<div class="wpec-single-note-meta">'    +
+							'<span title="Added by '+response.data.note.admin_name+'">added on '+response.data.note.note_date+'</span>'+
+							'<a href="#" class="delete-order-note" data-orderid="'+response.data.note.order_id+'" data-note-id="' + response.data.note.id + '">Delete</a>'+
+						'</div>'+						
+                            '</div>';
+                        $('#admin-order-notes').append(noteRow);
+                    } else {
+                        // Handle error response
+                        console.error(response.error);
+                    }
+                },
+                error: function(jqXHR, textStatus, errorThrown) {
+					btn.val("Add");
+					btn.prop("disabled",false);
+                    // Handle Ajax error
+                    console.error(textStatus, errorThrown);
+                }
+            });
+        }
+    });
+
+    // Delete order note
+    $(document).on('click', '.delete-order-note', function(e) {
+        e.preventDefault();
+
+		var btn = $(this);
+        if (confirm('Are you sure you want to delete this order note?')) {
+            var noteId = $(this).data('note-id');
+			var order_id = $(this).data('orderid');
+
+			btn.prop("disabled",true);
+			btn.text("deleting");
+
+            if (noteId !== '') {
+                var data = {
+                    action: 'wpec_delete_order_note',
+                    note_id: noteId,
+					nonce: wpecAdminSideVars.delete_order_note_nonance,
+					order_id: order_id
+                };
+
+                $.ajax({
+                    url: wpecAdminSideVars.ajaxurl,
+                    type: 'POST',
+                    data: data,
+                    success: function(response) {
+						btn.prop("disabled",false);
+						btn.text("Delete");
+                        // Handle success response
+                        if (response.success) {
+                            // Remove the deleted note row from the table
+                            $(e.target).closest('.wpec-single-note').remove();
+                        } else {
+                            // Handle error response
+                            console.error(response.error);
+                        }
+                    },
+                    error: function(jqXHR, textStatus, errorThrown) {
+						btn.prop("disabled",false);
+						btn.text("Delete");
+                        // Handle Ajax error
+                        console.error(textStatus, errorThrown);
+                    }
+                });
+            }
+        }
+    });
+
 	$( '.wpec-order-action' ).on( 'click', function( e ) {
 		e.preventDefault();
 		var button = $( this );
