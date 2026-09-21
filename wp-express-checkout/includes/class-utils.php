f0caf29797a887fa40cebb53533facea091fdadb
adilrabid <rabid1819@gmail.com>
2026-09-21 19:41:37 +0600

    Can't select membership level issue in product edit page has fixed.

--- wp-express-checkout/includes/class-utils.php
+++ wp-express-checkout/includes/class-utils.php
@@ -793,6 +793,61 @@ class Utils {
 		return $tpl_html;
 	}
 
+	public static function wp_kses_common_attributes() {
+		$common_attributes = array(
+			'id'       => true,
+			'class'    => true,
+			'style'    => true,
+			'title'    => true,
+			'disabled' => true,
+			'aria-*'   => true,
+			'data-*'   => true,
+		);
+		return apply_filters( 'wpec_kses_common_attributes', $common_attributes );
+	}
+
+	public static function wp_kses_select_option_tags(){
+
+		$common_attributes = self::wp_kses_common_attributes();
+
+		$allowed_tags = array(
+			'option' => array_merge( $common_attributes, array(
+				'value'    => true,
+				'label'    => true,
+				'selected' => true,
+			) ),
+
+			'optgroup' => array_merge( $common_attributes, array(
+				'label' => true,
+			) ),
+		);
+
+		return apply_filters( 'wpec_kses_select_option_tags', $allowed_tags );
+	}
+
+	/**
+	 * Returns allowed select field tags and attributes for wp_kses().
+	 *
+	 * @return array Allowed HTML tags and attributes.
+	 */
+	public static function wp_kses_select_tags() {
+		$allowed_tags = array(
+			'select' => array_merge( self::wp_kses_common_attributes(), array(
+				'name'         => true,
+				'multiple'     => true,
+				'required'     => true,
+				'size'         => true,
+				'form'         => true,
+				'autocomplete' => true,
+				'autofocus'    => true,
+				'tabindex'     => true,
+			) ),
+		);
+		$allowed_tags = array_merge( $allowed_tags, self::wp_kses_select_option_tags() );
+
+		return apply_filters( 'wpec_kses_select_tags', $allowed_tags );
+	}
+
 	public static function wp_kses_post_tags_with_form() {
 		// Array of common standard and custom form attributes
 		$all_attributes = array(
